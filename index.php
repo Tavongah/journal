05a9f82cdb1309b924ff4e2a548d2a80c7f07913
Tavongah <tavongadutumah@gmail.com>
2024-04-23 11:36:56 +0200

    journal

--- index.php
+++ index.php
@@ -9,21 +9,33 @@ include_once 'dbconnect.php';
     <title>Upload View & Download file in PHP and MySQL | Demo</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport" >
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" >
+
+    <style>
+    .custom-file-input:lang(en)~.custom-file-label:after {
+      content: "Browse";
+    }
+  </style>
 </head>
 <body>
 <br/>
-<div class="container">
+
+<div class="container mt-5">
+    <center><h2 class="mb-4">Journal Upload</h2></center>
     <div class="row">
-        <div class="col-xs-8 col-xs-offset-2 well">
-        <form action="upload.php" method="post" enctype="multipart/form-data">
-            <legend>Select File to Upload:</legend>
-            <div class="form-group">
-                <input type="file" name="file1" />
-            </div>
-            <div class="form-group">
-                <input type="submit" name="submit" value="Upload" class="btn btn-info"/>
-            </div>
-            <?php if(isset($_GET['st'])) { ?>
+    <div class="col-md-6 offset-md-3">
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+    
+      <div class="custom-file mb-3">
+      <div class="form-group">
+        <input type="file" name="file1" class="custom-file-input" id="customFile">
+     </div>
+        
+      </div>
+      <div class="form-group">
+      <button type="submit" name="submit" value="Upload" class="btn btn-primary">Upload</button>
+      </div>
+       
+      <?php if(isset($_GET['st'])) { ?>
                 <div class="alert alert-danger text-center">
                 <?php if ($_GET['st'] == 'success') {
                         echo "File Uploaded Successfully!";
@@ -34,11 +46,39 @@ include_once 'dbconnect.php';
                     } ?>
                 </div>
             <?php } ?>
+    </form>
+    </div>
+    </div>
+  </div>
+
+
+<!--<div class="container">
+    <div class="row">
+        <div class="col-xs-8 col-xs-offset-2 well">
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <legend>Select File to Upload:</legend>
+            <div class="form-group">
+                <input type="file" name="file1" />
+            </div>
+            <div class="form-group">
+                <input type="submit" name="submit" value="Upload" class="btn btn-info"/>
+            </div>
+            <?php //if(isset($_GET['st'])) { ?>
+                <div class="alert alert-danger text-center">
+                <?php // if ($_GET['st'] == 'success') {
+                       // echo "File Uploaded Successfully!";
+                    //}
+                    //else
+                    //{
+                      //  echo 'Invalid File Extension!';
+                   // } ?>
+                </div>
+            <?php// } ?>
         </form>
         </div>
     </div>
     
-</div>
+</div>-->
 
 
 </body>
